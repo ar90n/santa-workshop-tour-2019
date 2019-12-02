b45fe57f2084471b34de3b415c0fa4325b3f0481
Masahiro Wada <argon.argon.argon@gmail.com>
2019-12-02 03:08:30 +0000

    fix typo

--- santa_workshop_tour_2019/io.py
+++ santa_workshop_tour_2019/io.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 
 def load_data(root: Path = Path("../input")):
-    path = root / "santa-2019-workshop-scheduling" / "family_data.csv"
+    path = root / "santa-workshop-tour-2019" / "family_data.csv"
     return pd.read_csv(path, index_col='family_id')
 
 def load_sample_submission(root: Path = Path("../input")):
-    path = root / "santa-2019-workshop-scheduling" / "sample_submission.csv"
+    path = root / "santa-workshop-tour-2019" / "sample_submission.csv"
     return pd.read_csv(path, index_col='family_id')
