11b0159493f11bc9257f580354c4874b739742b1
Masahiro Wada <argon.argon.argon@gmail.com>
2019-12-10 21:36:50 +0000

    Add family_size lap

--- santa_workshop_tour_2019/lap.py
+++ santa_workshop_tour_2019/lap.py
@@ -15,12 +15,6 @@ def _calc_days(occupancies, col, l):
         best[i] += j
     return best
 
-def solve(data, occupancies):
-    weights = _get_weights(data, occupancies)
-    least_cost, col, row = lapjv(weights)
-
-    return _calc_days(occupancies, col, weights.shape[0])
-
 
 def _get_weights(data, occupancies):
     weights = create_penalty_memo(data)
@@ -29,3 +23,55 @@ def _get_weights(data, occupancies):
     weights = weights.reshape(5000, 5000)
 
     return weights
+
+def build_family_lap(data):
+    family_size = data.n_people.values
+    weights_source = create_penalty_memo(data)[:, 1:].reshape(5000, 100)
+
+    def _family_lap(data, occupancies):
+        weights = weights_source.repeat(occupancies[1:], axis=1)
+        _, col, _ = lapjv(weights)
+        best = _calc_days(occupancies, col, weights.shape[0])
+
+        daily_occupancy = np.zeros(101, dtype=np.int64)
+        for j in range(len(best):
+            daily_occupancy[best[j]] += family_size[j]
+
+        return best, daily_occupancy
+
+    return _family_lap
+
+
+def build_family_size_lap(data):
+    family_size = data.n_people.values
+    penalty_memo = create_penalty_memo(data)
+
+    fams = {}
+    for fam_id in range(len(family_size)):
+        fam_size = family_size[fam_id]
+        fams.setdefault(fam_size, []).append(fam_id)
+
+    @njit
+    def _create_weights(prediction, fam_ids):
+        weights = np.zeros((len(fam_ids), len(fam_ids)), dtype=np.int64)
+        for i in range(len(fam_ids)):
+            for j in range(len(fam_ids)):
+                if i == j:
+                    continue
+                idi = fam_ids[i]
+                idj = fam_ids[j]
+                di = prediction[idi]
+                dj = prediction[idj]
+                weights[i, j] = (penalty_memo[idi, dj] - penalty_memo[idi, di])
+        return weights
+
+    def _family_size_lap(prediction):
+        new = prediction.copy()
+        for fam_ids in fams.values():
+            weights = _create_weights(prediction, fam_ids)
+            _, col, _ = lapjv(weights)
+
+            for i in range(len(col)):
+                new[fam_ids[i]] = prediction[fam_ids[col[i]]]
+        return new
+    return _family_size_lap
--- scripts/main.py
+++ scripts/main.py
@@ -1,29 +1,21 @@
+import numpy as np
 from santa_workshop_tour_2019 import io
-from santa_workshop_tour_2019.cost import build_cost_function
-from santa_workshop_tour_2019.const import cols, days
-from santa_workshop_tour_2019.lap import solve as solve_lap
+from santa_workshop_tour_2019.cost import build_cost_function, create_penalty_memo
+from santa_workshop_tour_2019.const import FLAT_SLOTS
+from santa_workshop_tour_2019.lap import build_family_lap, build_family_size_lap
 from santa_workshop_tour_2019.optim import build_greedy_move_func, build_greedy_swap_func, stochastic_product_search
 
 data = io.load_data()
 
-import numpy as np
-occupancies = np.zeros(101, dtype=np.int)
-occupancies[1:] = 50
-best = solve_lap(data, occupancies)
 
-
-i = 0
 total_cost, delta_move_cost, delta_swap_cost = build_cost_function(data)
-
-family_size = data.n_people.values
-N = len(best)
-daily_occupancy = np.zeros(101, dtype=np.int64)
-for j in range(N):
-    daily_occupancy[best[j]] += family_size[j]
-
 greedy_move = build_greedy_move_func(data, delta_move_cost)
 greedy_swap = build_greedy_swap_func(data, delta_swap_cost)
+family_lap = build_family_lap(data)
+family_size_lap = build_family_size_lap(data)
 
+i = 0
+best, daily_occupancy = family_lap(data, FLAT_SLOTS)
 while i <= 15:
     best, daily_occupancy = greedy_move(best, daily_occupancy)
     score = total_cost(best)
@@ -32,6 +24,11 @@ while i <= 15:
     best, daily_occupancy = greedy_swap(best, daily_occupancy)
     score = total_cost(best)
     print(f"Score1: {score}")
+
+    best = family_size_lap(best)
+    score = total_cost(best)
+    print(f"Score2: {score}")
+
     i += 1
 
 io.save_result(best, score)
