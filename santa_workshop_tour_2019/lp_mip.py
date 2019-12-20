e92791a6d1f00b9a0e88d4b5a01aff1c2c707bcf
Masahiro Wada <argon.argon.argon@gmail.com>
2019-12-20 17:30:47 +0000

    refactor

--- santa_workshop_tour_2019/lp_mip.py
+++ santa_workshop_tour_2019/lp_mip.py
@@ -4,14 +4,14 @@ from ortools.linear_solver import pywraplp
 from .const import N_DAYS, N_FAMILIES, MAX_OCCUPANCY, MIN_OCCUPANCY
 from .cost import create_penalty_memo, create_accounting_memo
 
-#N_DAYS, N_FAMILIES, MAX_OCCUPANCY, MIN_OCCUPANCY = 5, 10, 8, 2
+# N_DAYS, N_FAMILIES, MAX_OCCUPANCY, MIN_OCCUPANCY = 5, 10, 8, 2
 
-NumThreads = 4
 
 def _add_delta_occupancy_constraint(S, occupancy, delta):
     for j in range(len(occupancy) - 1):
         S.Add(occupancy[j] - occupancy[j + 1] <= delta)
         S.Add(occupancy[j + 1] - occupancy[j] <= delta)
+
 
 def _add_penalty_candidates(S, DESIRED):
     x = {}
@@ -23,14 +23,21 @@ def _add_penalty_candidates(S, DESIRED):
             x[i, j] = S.BoolVar("x[%i,%i]" % (i, j))
     return x, candidates
 
+
 def _add_accounting_candidates(S, candidates, occupancy, th, accounting_memo):
     y = {}
     for d, cs in candidates.items():
-        vs = range(MIN_OCCUPANCY, MAX_OCCUPANCY + 1) if (d == (N_DAYS-1) or (d + 1) in candidates) else [occupancy[d+1]]
+        vs = (
+            range(MIN_OCCUPANCY, MAX_OCCUPANCY + 1)
+            if (d == (N_DAYS - 1) or (d + 1) in candidates)
+            else [occupancy[d + 1]]
+        )
         for u in range(MIN_OCCUPANCY, MAX_OCCUPANCY + 1):
             for v in vs:
                 if accounting_memo[u, v] <= th:
-                    y.setdefault(d, {}).update({(u, v):S.BoolVar("y[%i,%i,%i]" % (d, u, v))})
+                    y.setdefault(d, {}).update(
+                        {(u, v): S.BoolVar("y[%i,%i,%i]" % (d, u, v))}
+                    )
     return y
 
 
@@ -43,19 +50,19 @@ def _calc_preference_cost(S, x, DESIRED, penalty_memo):
         [penalty_memo[i, j + 1] * x[i, j] for i, days in DESIRED.items() for j in days]
     )
 
+
 def _calc_accounting_cost(S, y, accounting_memo):
     days = set(y.keys())
     return S.Sum(
-        [
-            accounting_memo[u, v] * yv
-            for d in days for (u, v), yv in y[d].items()
-        ]
+        [accounting_memo[u, v] * yv for d in days for (u, v), yv in y[d].items()]
     )
+
 
 def _add_family_presence_constraint(S, x, DESIRED):
     family_presence = _calc_faily_presence(S, x, DESIRED)
     for i in range(len(family_presence)):
         S.Add(family_presence[i] == 1)
+
 
 def _add_occupancy_constraint(S, candidates, occupancy):
     for j in sorted(candidates.keys()):
@@ -65,18 +72,10 @@ def _add_occupancy_constraint(S, candidates, occupancy):
 
 def _add_accounting_constraint(S, y, occupancy):
     for d, yd in y.items():
-        y_sum_u = S.Sum(
-            [
-                yv * u for (u, _), yv in yd.items()
-            ]
-        )
+        y_sum_u = S.Sum([yv * u for (u, _), yv in yd.items()])
         S.Add(y_sum_u == occupancy[d])
 
-        y_sum_v = S.Sum(
-            [
-                yv * v for (_, v), yv in yd.items()
-            ]
-        )
+        y_sum_v = S.Sum([yv * v for (_, v), yv in yd.items()])
         S.Add(y_sum_v == occupancy[d + 1])
 
         y_sum = S.Sum(yd.values())
@@ -88,8 +87,20 @@ def _add_smoothness_constraint(S, y, candidates):
         if (d + 1) not in candidates:
             continue
         for t in range(MIN_OCCUPANCY, MAX_OCCUPANCY + 1):
-            y_sum_u = S.Sum([y[d][u, t] for u in range(MIN_OCCUPANCY, MAX_OCCUPANCY + 1) if (u,t) in y[d]])
-            y_sum_v = S.Sum([y[d + 1][t, v] for v in range(MIN_OCCUPANCY, MAX_OCCUPANCY + 1) if (t,v) in y[d+1]])
+            y_sum_u = S.Sum(
+                [
+                    y[d][u, t]
+                    for u in range(MIN_OCCUPANCY, MAX_OCCUPANCY + 1)
+                    if (u, t) in y[d]
+                ]
+            )
+            y_sum_v = S.Sum(
+                [
+                    y[d + 1][t, v]
+                    for v in range(MIN_OCCUPANCY, MAX_OCCUPANCY + 1)
+                    if (t, v) in y[d + 1]
+                ]
+            )
             S.Add(y_sum_u == y_sum_v)
 
 
@@ -103,6 +114,7 @@ def _calc_occupancy(S, x, prediction, occupancy, candidates, family_size):
     occupancy.append(occupancy[-1])
     return occupancy
 
+
 def _get_solver_result(res):
     return {
         0: "OPTIMAL",
@@ -114,6 +126,7 @@ def _get_solver_result(res):
         6: "NOT_SOLVED",
     }[res]
 
+
 def _get_result_df(DESIRED, x):
     l = [
         (i, j, x[i, j].solution_value())
@@ -123,14 +136,12 @@ def _get_result_df(DESIRED, x):
     ]
     return pd.DataFrame(l, columns=["family_id", "day", "n"])
 
+
 def solveSantaLP(DESIRED, family_size, penalty_memo, accounting_memo):
 
     S = pywraplp.Solver(
         "SolveAssignmentProblem", pywraplp.Solver.GLOP_LINEAR_PROGRAMMING
     )
-
-    S.SetNumThreads(NumThreads)
-    # S.set_time_limit(limit_in_seconds*1000*NumThreads) #cpu time = wall time * N_threads
 
     x, candidates = _add_penalty_candidates(S, DESIRED)
     occupancy = _calc_occupancy(S, x, {}, [0] * N_DAYS, candidates, family_size)
@@ -151,6 +162,7 @@ def solveSantaLP(DESIRED, family_size, penalty_memo, accounting_memo):
 
     return _get_result_df(DESIRED, x)
 
+
 def solveSantaIP(
     prediction, DESIRED, daily_occupancy, th, family_size, penalty_memo, accounting_memo
 ):
@@ -159,11 +171,10 @@ def solveSantaIP(
         "SolveAssignmentProblem", pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING
     )
 
-    S.SetNumThreads(NumThreads)
-    # S.set_time_limit(limit_in_seconds*1000*NumThreads) #cpu time = wall time * N_threads
-
     x, candidates = _add_penalty_candidates(S, DESIRED)
-    occupancy = _calc_occupancy(S, x, prediction, daily_occupancy, candidates, family_size)
+    occupancy = _calc_occupancy(
+        S, x, prediction, daily_occupancy, candidates, family_size
+    )
     y = _add_accounting_candidates(S, candidates, occupancy, th, accounting_memo)
 
     # Objective
@@ -191,7 +202,7 @@ def build_lp_mip(data):
     family_size = data.n_people.values
     penalty_memo = create_penalty_memo(data)
     accounting_memo = create_accounting_memo()
-    DESIRED = {i: data.values[i, :-1] -1 for i in range(data.shape[0])}
+    DESIRED = {i: data.values[i, :-1] - 1 for i in range(data.shape[0])}
 
     def solveSanta():
         df = solveSantaLP(
@@ -209,7 +220,13 @@ def build_lp_mip(data):
         occupancy = assigned_df.groupby("day").family_size.sum().values
 
         rdf = solveSantaIP(
-            predictions, unassigned, occupancy, 1024, family_size, penalty_memo, accounting_memo
+            predictions,
+            unassigned,
+            occupancy,
+            1024,
+            family_size,
+            penalty_memo,
+            accounting_memo,
         )  # solve the rest with MIP
         df = pd.concat((assigned_df, rdf)).sort_values("family_id")
         return df.day.values + 1
