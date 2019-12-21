33efd509270de19d720b27826a33a413e499ceae
Masahiro Wada <argon.argon.argon@gmail.com>
2019-12-21 11:59:12 +0000

    increate loop

--- scripts/main.py
+++ scripts/main.py
@@ -26,7 +26,7 @@ score, daily_occupancy = total_cost(best)
 best_score = score
 print(f"Score0: {score}")
 
-while i <= 80:
+while i <= 100:
     best, daily_occupancy = greedy_move(best, daily_occupancy)
     score, daily_occupancy = total_cost(best)
     print(f"Score1: {score}")
@@ -42,7 +42,7 @@ while i <= 80:
         score, daily_occupancy = total_cost(best)
         print(f"Score3.{s}: {score}")
 
-    for j in range(3):
+    for j in range(5):
         best = mip(best, daily_occupancy)
         score, daily_occupancy = total_cost(best)
         print(f"Score4.{j}: {score}")
