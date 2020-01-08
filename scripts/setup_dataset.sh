b04ff071bd601fad333e660ad8aadff4954c5d2d
Masahiro Wada <argon.argon.argon@gmail.com>
2020-01-08 20:04:34 +0000

    Add download script

---
+++ scripts/setup_dataset.sh
@@ -0,0 +1,3 @@
+mkdir -p ../input/santa2019work/
+poetry run kaggle d download ar90ngas/santa2019work -p ../input/santa2019work/
+unzip ../input/santa2019work/santa2019work.zip -d ../input/santa2019work
