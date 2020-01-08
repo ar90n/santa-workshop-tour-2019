2d46bf9bf081ad3c665194a930d167897621a40e
Masahiro Wada <argon.argon.argon@gmail.com>
2020-01-08 20:15:16 +0000

    Improve setup script

--- scripts/setup_dataset.sh
+++ scripts/setup_dataset.sh
@@ -1,3 +1,8 @@
 mkdir -p ../input/santa2019work/
 poetry run kaggle d download ar90ngas/santa2019work -p ../input/santa2019work/
 unzip ../input/santa2019work/santa2019work.zip -d ../input/santa2019work
+
+
+mkdir -p ../input/santa-workshop-tour-2019
+poetry run kaggle c download -c santa-workshop-tour-2019 -p ../input/santa-workshop-tour-2019
+unzip ../input/santa-workshop-tour-2019/santa-workshop-tour-2019.zip -d ../input/santa-workshop-tour-2019
